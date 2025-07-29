a8e88f2bccfb7e8fd9297f33fcc4510a52c4d75e
sarwar hussain <sarwar@sarwars-Mac-mini.local>
2025-07-30 00:07:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// <!-- build a passenger counter app using js -->
+
+// document.getElementById("count-el").innerText = 5;
+
+//projects
+let countEl = document.getElementById("count-el");
+
+console.log(countEl);
+
+let count = 0;
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+  console.log(count);
+}
