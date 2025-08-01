0f73cb46b6bf9701bb09ed6119b1a4fcaf51f012
sarwar hussain <sarwar@sarwars-Mac-mini.local>
2025-08-01 15:41:07 +0600

    second commit

--- index.js
+++ index.js
@@ -4,12 +4,14 @@
 
 //projects
 let countEl = document.getElementById("count-el");
-
 console.log(countEl);
 
 let count = 0;
 function increment() {
   count += 1;
   countEl.innerText = count;
+}
+
+function save() {
   console.log(count);
 }
